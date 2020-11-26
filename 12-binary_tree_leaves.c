8a9253e1cce4df2f7b806f7b299f9d8b088d5685
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-26 09:08:43 +0000

    current file

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,8 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - create a node
- * @parent: pointer to parent the new node
- * @value: value to new node
- * Return: return my node
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: leaves left and rigth
  */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int l_leaves, rleaves;
+
+	if (!tree)
+		return (0);
+
+	l_leaves = binary_tree_leaves(tree->left);
+	rleaves = binary_tree_leaves(tree->right);
+
+	if (!tree->left && !tree->right)
+		return (1 + l_leaves + rleaves);
+	return (l_leaves + rleaves);
+}
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,8 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - create a node
- * @parent: pointer to parent the new node
- * @value: value to new node
- * Return: return my node
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ * Return: return nodes left + rigth
  */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int l_nodes, r_nodes;
+
+	if (!tree)
+		return (0);
+
+	l_nodes = binary_tree_nodes(tree->left);
+	r_nodes = binary_tree_nodes(tree->right);
+
+	if (tree->left || tree->right)
+		return (1 + l_nodes + r_nodes);
+	return (l_nodes + r_nodes);
+}
