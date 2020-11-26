6b578cd78287396fd5c02a24b9a1886726c0c282
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-26 05:46:30 +0000

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,11 +7,9 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t i = 0:
+	size_t i = 0;
 
-	if (!tree)
-		return (0);
-	if (!tree->parent)
+	if (!tree || !tree->parent)
 		return (0);
 
 	while (tree->parent)
