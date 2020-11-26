cb2ed56fa859abe2dd6b9be087f66b1a3877f528
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-26 02:51:53 +0000

    current file

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - enter the rigth node
+ * binary_tree_insert_right - enter the rigth node
  * @parent: pointer to parent the new node
  * @value: value to new node
  * Return: return my node
