1b1b797343c864d5e494b5af2e2703b9335fe36f
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-26 00:23:52 +0000

    first node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,8 +1,10 @@
 #include "binary_trees.h"
+
 /**
- *
- *
- *
+ * binary_tree_node - create a node
+ * @parent: pointer to parent the new node
+ * @value: value to new node
+ * Return: return my node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
