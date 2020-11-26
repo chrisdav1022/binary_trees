4f7ba90362d67d59544b9b87a9eb6a3e0fe059be
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-26 04:41:16 +0000

    heigth

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
-	left_me = 1 + binary_tree_height(tree->left);
+	left_me = binary_tree_height(tree->left);
 	right_me = binary_tree_height(tree->right);
 
 	if (left_me > right_me)
