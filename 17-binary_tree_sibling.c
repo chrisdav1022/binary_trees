3cd433507e39306710e462d6d6855da9421cad19
Cristian Bedoya <chrisbedoya8@gmail.com>
2020-11-25 21:42:33 -0500

    current file

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,8 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a node
+ * @parent: pointer to parent the new node
+ * @value: value to new node
+ * Return: return my node
+ */
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - enter the left node
+ * @parent: pointer to parent the new node
+ * @value: value to new node
+ * Return: return my node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *lnode;
+
+	if (!parent)
+		return(NULL);
+
+	lnode = malloc(sizeof(binary_tree_t));
+
+	if (!lnode)
+		return (NULL);
+
+	lnode->n = value;
+	lnode->parent = parent;
+	lnode->left = NULL;
+	lnode->right = NULL;
+
+    if (parent->right != NULL)
+	{
+		lnode->right = parent->right;
+		parent->right->parent = lnode;
+	}
+	parent->right = lnode;
+	return (lnode);
+
+}
