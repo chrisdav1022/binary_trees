d7ca776663c6924f12f972bdcd0924420c3a5896
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-26 00:13:27 +0000

    current file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,18 +6,17 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *mynode;
 
-	if (node == NULL)
+	mynode = malloc(sizeof(binary_tree_t));
+
+	if (mynode == NULL)
 		return (NULL);
 
-	node = malloc(sizeof(binary_tree_t));
+	mynode->n = value;
+	mynode->parent = parent;
+	mynode->left = NULL;
+	mynode->right = NULL;
 
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
-
-	return (node);
-
+	return (mynode);
 }
