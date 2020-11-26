7b0ecc6a158847af63abc19bb4611b3564a189c3
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-26 09:53:16 +0000

    full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,8 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - create a node
- * @parent: pointer to parent the new node
- * @value: value to new node
- * Return: return my node
+ * binary_tree_is_full - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance
+ * Return: 1 full 0 fail
  */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+	else if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	return (0);
+}
